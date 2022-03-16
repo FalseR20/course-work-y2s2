d8fda40de2906bb245a4bd12a4a04b09c680bc32
FalseR20 <kno3.2002@gmail.com>
2022-03-16 21:54:31 +0300

    feat: add thieving working lab

--- main.cpp
+++ main.cpp
@@ -1,27 +1,73 @@
-#include <mpi.h>
-#include <cstdio>
+#include <iostream>
+#include "turbojpeg.h"
 
-int main() {
-    // Initialize the MPI environment
-    MPI_Init(nullptr, nullptr);
+const char* FILENAME = "..\\jpg\\image3.jpg";
+const char* FILENAME_NEW = "..\\jpg\\new_image3.jpg";
 
-    // Get the number of processes
-    int world_size;
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-    // Get the rank of the process
-    int world_rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+unsigned char* get_image_solution(int &height, int &width, unsigned long& jpegSize, unsigned char* jpegBuf){
+    FILE *file = fopen(FILENAME, "rb");
+    int jpegSubsamp = TJSAMP_GRAY; //
 
-    // Get the name of the processor
-    char processor_name[MPI_MAX_PROCESSOR_NAME];
-    int name_len;
-    MPI_Get_processor_name(processor_name, &name_len);
+    fseek(file, 0, SEEK_END); // set pointer to the end of file for getting file size
+    jpegSize = ftell(file); // get file size
+    fseek(file, 0, SEEK_SET);
+    jpegBuf = new unsigned char[jpegSize];
+    fread( jpegBuf, jpegSize, 1, file ); //get a pointer to a buffer containing JPEG image in jpegBuf
 
-    // Print off a hello world message
-    printf("Hello world from processor %s, rank %d out of %d processors\n",
-           processor_name, world_rank, world_size);
+    tjhandle handle = tjInitDecompress();
+    tjDecompressHeader2(handle, jpegBuf, jpegSize, &width, &height, &jpegSubsamp);
+    fclose(file);
+    return jpegBuf;
+}
 
-    // Finalize the MPI environment.
-    MPI_Finalize();
+unsigned char* get_grayscale(int height, int width, unsigned long jpegSize,unsigned char* jpegBuf, unsigned char* image){
+    image = new unsigned char[width * height];
+    tjhandle handle = tjInitDecompress();
+    int pixelFormat = TJPF_GRAY;
+    int nbands = 1;
+    int flags = 0;
+    int pitch = width * nbands;
+    tjDecompress2(handle, jpegBuf, jpegSize, image, width, pitch, height, pixelFormat, flags);
+    return image;
+}
+
+unsigned char* robberts(int height, int width, unsigned char* image){
+
+    unsigned char* new_image = new unsigned char[width * height];
+    for (int i = 0; i < height-1; i++) {
+        for (int j = 0; j < width; j++) {
+            int new_pixel = abs((image[i*width+j]-image[(i+1)*width+(j+1)]))+
+                            abs((image[i*width+(j+1)]-image[(i+1)*width+j]));
+            new_image[i * width + j] = new_pixel;
+        }
+    }
+    return new_image;
+}
+int main(){
+    int height, width;
+    unsigned long jpegSize;
+    unsigned char *jpegBuf;
+    jpegBuf = get_image_solution(height, width, jpegSize, jpegBuf);//getting file
+    unsigned char *image;
+    image = get_grayscale(height, width, jpegSize, jpegBuf, image);
+
+
+
+
+    tjhandle handle_2 = tjInitCompress();
+
+    unsigned char * jpegBuf_2 = NULL;
+    unsigned long jpegSize_2 = 0;
+
+
+    tjCompress2(handle_2, robberts(height,width,image), width, width, height, TJPF_GRAY, &(jpegBuf_2), &jpegSize_2,
+                TJSAMP_GRAY , 91, 0);
+    FILE *file_2 = fopen(FILENAME_NEW, "wb");
+
+    fwrite(jpegBuf_2, jpegSize_2, 1, file_2);
+
+
+
+
 }
